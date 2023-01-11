3763a4bf58b518558dcd8edb310fececd092ea0c
Adilsov911 <adiles@code.edu.az>
2023-01-12 00:43:45 +0400

    HomePage

---
+++ assest/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.Slider').slick({
+      
+        infinite: true,
+        speed: 1650,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow:'<span class="priv-arrow"><i class="fa-solid fa-angle-left"></i></span>',
+        nextArrow:'<span class="next-arrow"><i class="fa-solid fa-chevron-right"></i></span>'
+      });
+});
