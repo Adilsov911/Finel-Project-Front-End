dbe1e32a86a12f567258dd932b8d1d386e628d27
Adilsov911 <adiles@code.edu.az>
2023-01-19 02:35:25 +0400

    front-end 97% ready

--- assest/js/main.js
+++ assest/js/main.js
@@ -788,3 +788,14 @@ $(document).ready(function() {
     return false;
   });
 });
+
+$('.button').click(function(){
+  var buttonId = $(this).attr('id');
+  $('#modal-container').removeAttr('class').addClass(buttonId);
+  $('body').addClass('modal-active');
+})
+
+$('#modal-container').click(function(){
+  $(this).addClass('out');
+  $('body').removeClass('modal-active');
+});
